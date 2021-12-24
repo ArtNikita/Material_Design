167ad80831a9c68c5853c8fb8b7e246a795f514c
Nikita Artamonov <nikitaartamonov@mail.ru>
2021-12-24 23:29:46 +0300

    homework1: added view binding to project

--- app/src/main/java/ru/nikitaartamonov/materialdesign/MainActivity.kt
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.nikitaartamonov.materialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.nikitaartamonov.materialdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
