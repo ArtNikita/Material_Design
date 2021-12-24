78cc8c5d3e514f309e9482c4f9cb9831b8e66239
Nikita Artamonov <nikitaartamonov@mail.ru>
2021-12-25 00:09:49 +0300

    homework1: added MainViewModel, added ui folder

--- app/src/main/java/ru/nikitaartamonov/materialdesign/ui/main/MainActivity.kt
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/ui/main/MainActivity.kt
@@ -1,15 +1,24 @@
-package ru.nikitaartamonov.materialdesign
+package ru.nikitaartamonov.materialdesign.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import ru.nikitaartamonov.materialdesign.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MainViewModel>()
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViewModel()
+        viewModel.onViewIsReady()
+    }
+
+    private fun initViewModel() {
+        //todo
     }
 }
---
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/ui/main/MainViewModel.kt
@@ -0,0 +1,11 @@
+package ru.nikitaartamonov.materialdesign.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    fun onViewIsReady() {
+
+    }
+
+}
