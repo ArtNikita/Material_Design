f05744366198f8b24ec3b25603f290004d22f68b
Nikita Artamonov <nikitaartamonov@mail.ru>
2021-12-25 04:31:34 +0300

    homework1: saving bottom sheet state via viewModel now

--- app/src/main/java/ru/nikitaartamonov/materialdesign/ui/main/MainActivity.kt
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/ui/main/MainActivity.kt
@@ -1,9 +1,12 @@
 package ru.nikitaartamonov.materialdesign.ui.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import ru.nikitaartamonov.materialdesign.data.retrofit.ImageWrapper
 import ru.nikitaartamonov.materialdesign.databinding.ActivityMainBinding
 
@@ -11,11 +14,14 @@ class MainActivity : AppCompatActivity() {
     private val viewModel by viewModels<MainViewModel>()
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initBottomSheet()
         initViewModel()
         viewModel.onViewIsReady()
     }
@@ -23,6 +29,9 @@ class MainActivity : AppCompatActivity() {
     private fun initViewModel() {
         viewModel.renderImageDataLiveData.observe(this) {
             renderImageData(it)
+        }
+        viewModel.bottomSheetStateLiveData.observe(this) { currentState ->
+            bottomSheetBehavior.state = currentState
         }
     }
 
@@ -33,5 +42,18 @@ class MainActivity : AppCompatActivity() {
             .into(binding.dailyImageView)
         binding.bottomSheet.titleTextView.text = imageWrapper.title
         binding.bottomSheet.copyrightTextView.text = imageWrapper.copyright
+    }
+
+    private fun initBottomSheet() {
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet.bottomSheetContainer)
+        bottomSheetBehavior.addBottomSheetCallback(
+            object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    viewModel.onBottomSheetStateChanged(newState)
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+            }
+        )
     }
 }
--- app/src/main/java/ru/nikitaartamonov/materialdesign/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/ui/main/MainViewModel.kt
@@ -3,6 +3,7 @@ package ru.nikitaartamonov.materialdesign.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import ru.nikitaartamonov.materialdesign.data.retrofit.ImageLoaderRetrofit
 import ru.nikitaartamonov.materialdesign.data.retrofit.ImageWrapper
 import ru.nikitaartamonov.materialdesign.domain.ImageLoader
@@ -10,11 +11,18 @@ import ru.nikitaartamonov.materialdesign.domain.ImageLoader
 class MainViewModel : ViewModel() {
     private val imageLoader: ImageLoader = ImageLoaderRetrofit()
     private var imageWrapper: ImageWrapper? = null
+    private var bottomSheetCurrentState = BottomSheetBehavior.STATE_COLLAPSED
 
     val renderImageDataLiveData: LiveData<ImageWrapper> = MutableLiveData()
+    val bottomSheetStateLiveData: LiveData<Int> = MutableLiveData()
 
     fun onViewIsReady() {
         loadImage()
+        bottomSheetStateLiveData.postValue(bottomSheetCurrentState)
+    }
+
+    fun onBottomSheetStateChanged(newState: Int) {
+        bottomSheetCurrentState = newState
     }
 
     private fun loadImage() {
