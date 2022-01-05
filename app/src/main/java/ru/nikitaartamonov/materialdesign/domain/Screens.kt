31b6091ea75b973499a92e126f074d9f0a6bb63c
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-01-05 03:11:55 +0300

    homework3: implemented bottom nav view listeners

---
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/domain/Screens.kt
@@ -0,0 +1,19 @@
+package ru.nikitaartamonov.materialdesign.domain
+
+import ru.nikitaartamonov.materialdesign.R
+import java.lang.IllegalStateException
+
+enum class Screens(val menuId: Int) {
+    DAILY_IMAGE(R.id.daily_image_fragment_menu),
+    EARTH_PHOTOS(R.id.earth_fragment_menu),
+    WEATHER(R.id.weather_fragment_menu);
+
+    companion object {
+        fun getById(id: Int): Screens {
+            for (screen in values()){
+                if (screen.menuId == id) return screen
+            }
+            throw IllegalStateException("No such menu id")
+        }
+    }
+}
--- app/src/main/java/ru/nikitaartamonov/materialdesign/ui/main/MainActivity.kt
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/ui/main/MainActivity.kt
@@ -3,7 +3,9 @@ package ru.nikitaartamonov.materialdesign.ui.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import ru.nikitaartamonov.materialdesign.databinding.ActivityMainBinding
+import ru.nikitaartamonov.materialdesign.domain.Screens
 import ru.nikitaartamonov.materialdesign.domain.Themes
 import ru.nikitaartamonov.materialdesign.ui.pages.daily_image.DailyImageFragment
 
@@ -19,7 +21,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
-            initStartFragment()
+            openScreen()
+        }
+        initViewModel()
+        initBottomNavigationView()
+    }
+
+    private fun initViewModel() {
+        viewModel.openScreenLiveData.observe(this) { event ->
+            event.getContentIfNotHandled()?.let { screen ->
+                openScreen(screen)
+            }
+        }
+    }
+
+    private fun openScreen(screen: Screens = Screens.DAILY_IMAGE) {
+        val fragmentToOpen: Fragment = when (screen) {
+            Screens.DAILY_IMAGE -> {
+                DailyImageFragment()
+            }
+            Screens.EARTH_PHOTOS -> {
+                //todo
+                DailyImageFragment()
+            }
+            Screens.WEATHER -> {
+                //todo
+                DailyImageFragment()
+            }
+        }
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.mainFragmentContainer.id, fragmentToOpen)
+            .commit()
+    }
+
+    private fun initBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            viewModel.bottomNavigationViewItemSelected(menuItem)
+            true
+        }
+        binding.bottomNavigationView.setOnItemReselectedListener { menuItem ->
+            viewModel.bottomNavigationViewItemReselected(menuItem)
         }
     }
 
@@ -29,12 +71,5 @@ class MainActivity : AppCompatActivity() {
             Themes.DEFAULT_THEME.toString()
         ) ?: Themes.DEFAULT_THEME.toString()
         return Themes.Entities.valueOf(currentTheme).styleId
-    }
-
-    private fun initStartFragment() {
-        supportFragmentManager
-            .beginTransaction()
-            .add(binding.mainFragmentContainer.id, DailyImageFragment())
-            .commit()
     }
 }
