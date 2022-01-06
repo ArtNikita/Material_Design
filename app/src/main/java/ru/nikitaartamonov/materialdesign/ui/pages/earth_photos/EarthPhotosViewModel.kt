043a868686f28d7f894124d24ffcfb0792f44ba3
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-01-06 04:03:21 +0300

    homework3: loading earth photos if not loaded yet

--- app/src/main/java/ru/nikitaartamonov/materialdesign/ui/pages/earth_photos/EarthPhotosViewModel.kt
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/ui/pages/earth_photos/EarthPhotosViewModel.kt
@@ -3,14 +3,34 @@ package ru.nikitaartamonov.materialdesign.ui.pages.earth_photos
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import ru.nikitaartamonov.materialdesign.app.App
+import ru.nikitaartamonov.materialdesign.data.retrofit.EarthPhotoWrapper
+import ru.nikitaartamonov.materialdesign.domain.EarthPhotosLoadingState
 import ru.nikitaartamonov.materialdesign.domain.ImageLoader
 
 class EarthPhotosViewModel : ViewModel() {
 
     private lateinit var imageLoader: ImageLoader
+    private var earthPhotos: List<EarthPhotoWrapper>? = null
 
     fun onViewIsReady(app: Application) {
         imageLoader = (app as App).imageLoader
+        if (earthPhotos == null) {
+            loadPhotos()
+        } else {
+            //todo
+        }
+    }
 
+    private fun loadPhotos() {
+        imageLoader.loadEarthPhotos { state ->
+            when (state) {
+                is EarthPhotosLoadingState.Success -> {
+                    //todo
+                }
+                is EarthPhotosLoadingState.Error -> {
+                    //todo
+                }
+            }
+        }
     }
 }
