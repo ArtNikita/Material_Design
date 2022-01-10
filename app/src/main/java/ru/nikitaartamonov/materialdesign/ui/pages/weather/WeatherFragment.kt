0ff81a09a9c77bc5bd6315d43abfc775137d5eae
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-01-10 22:07:32 +0300

    homework4: added collapsing toolbar

--- app/src/main/java/ru/nikitaartamonov/materialdesign/ui/pages/weather/WeatherFragment.kt
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/ui/pages/weather/WeatherFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bumptech.glide.Glide
 import ru.nikitaartamonov.materialdesign.R
 import ru.nikitaartamonov.materialdesign.databinding.FragmentWeatherBinding
 
@@ -15,9 +16,19 @@ class WeatherFragment : Fragment(R.layout.fragment_weather) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadToolbarImage()
         viewModel.onViewCreated(requireActivity().application)
         viewModel.setGstDataLiveData.observe(viewLifecycleOwner) { gstList ->
             binding.gstListTextView.text = gstList
         }
+    }
+
+    private fun loadToolbarImage() {
+        val imageUrl =
+            "https://images.pexels.com/photos/3617500/pexels-photo-3617500.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        Glide
+            .with(requireContext())
+            .load(imageUrl)
+            .into(binding.weatherToolbarImage)
     }
 }
