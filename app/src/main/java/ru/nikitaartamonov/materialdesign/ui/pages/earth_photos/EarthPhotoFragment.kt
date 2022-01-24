fcbc4db084db514d938f733c32ab0e68cfa42e28
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-01-24 15:32:57 +0300

    homework6: another small refactoring

--- app/src/main/java/ru/nikitaartamonov/materialdesign/ui/pages/earth_photos/EarthPhotoFragment.kt
+++ app/src/main/java/ru/nikitaartamonov/materialdesign/ui/pages/earth_photos/EarthPhotoFragment.kt
@@ -15,20 +15,17 @@ class EarthPhotoFragment : Fragment(R.layout.fragment_earth_photo) {
 
     private val binding by viewBinding(FragmentEarthPhotoBinding::bind)
 
-    private val args by lazy {
-        arguments ?: throw IllegalStateException("Arguments should be provided")
-    }
     private val imageLinkPart by lazy {
-        args.getString(IMAGE_LINK_PART_KEY)
+        requireArguments().getString(IMAGE_LINK_PART_KEY)
             ?: throw IllegalStateException("Image link should be provided")
     }
-    private val date by lazy { args.getString(DATE_KEY) }
+    private val dateOfPhoto by lazy { requireArguments().getString(DATE_KEY) }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setImage(imageLinkPart)
-        binding.earthPhotoDateTextView.text = date
+        binding.earthPhotoDateTextView.text = dateOfPhoto
     }
 
     private fun setImage(imageLinkPart: String) {
